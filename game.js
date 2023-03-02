f26ca14d4d3f7cf3fb5eb13ca53540f77237d038
Suru-web <itsmesuru60@gmail.com>
2023-03-02 23:52:34 +0530

    added a little timeout before starting

--- game.js
+++ game.js
@@ -9,71 +9,98 @@ var redsound = new Audio("./sounds/red.mp3");
 var yellowsound = new Audio("./sounds/yellow.mp3");
 var wrongsound = new Audio("./sounds/wrong.mp3")
 
-function nextSequence() {
-    clickedPattern = []
-    randomNumber = Math.floor(Math.random() * 4);
-    var randomPicked = colors[randomNumber];
-    colorPattern.push(randomPicked);
-    showAnimtion("#" + randomPicked)
-    level = colorPattern.length;
-    $("h1").text("Level " + level);
-    playSound(randomPicked);
-}
 
-function checkIfSubmitted() {
-    $(".start-butt").click(function () {
-        nextSequence();
-        $(".replay").slideUp();
-        $(".start-butt").fadeOut();
-    })
-}
-
-
-$("#green").click(function () {
-    showAnimtion("#green")
-    clickedPattern.push("green")
-    playSound("green");
-    checkSequence();
-})
-$("#red").click(function () {
-    showAnimtion("#red")
-    clickedPattern.push("red")
-    playSound("red");
-    checkSequence();
-})
-$("#yellow").click(function () {
-    showAnimtion("#yellow")
-    clickedPattern.push("yellow")
-    playSound("yellow");
-    checkSequence();
-})
-$("#blue").click(function () {
-    showAnimtion("#blue")
-    clickedPattern.push("blue")
-    playSound("blue");
-    checkSequence();
-})
-
-
-function checkSequence() {
-    var lol = 0;
-    for (var i = 0; i < clickedPattern.length; i++) {
-        if (colorPattern[i] != clickedPattern[i]) {
-            gameOver();
-            break;
-        }
-        else if (clickedPattern.length == colorPattern.length) {
-            lol = 1;
-        }
+function start() {
+    function nextSequence() {
+        clickedPattern = []
+        randomNumber = Math.floor(Math.random() * 4);
+        var randomPicked = colors[randomNumber];
+        colorPattern.push(randomPicked);
+        showAnimtion("#" + randomPicked)
+        level = colorPattern.length;
+        $("h1").text("Level " + level);
+        playSound(randomPicked);
     }
-    if (lol == 1)
+
+    function checkIfSubmitted() {
+        $(".start-butt").click(function () {
+            setTimeout(() => {
+                nextSequence();
+                $(".replay").slideUp();
+                $(".start-butt").fadeOut();
+            }, 500);
+        })
+    }
+
+
+    $("#green").click(function () {
+        showAnimtion("#green")
+        clickedPattern.push("green")
+        playSound("green");
+        checkSequence();
+    })
+    $("#red").click(function () {
+        showAnimtion("#red")
+        clickedPattern.push("red")
+        playSound("red");
+        checkSequence();
+    })
+    $("#yellow").click(function () {
+        showAnimtion("#yellow")
+        clickedPattern.push("yellow")
+        playSound("yellow");
+        checkSequence();
+    })
+    $("#blue").click(function () {
+        showAnimtion("#blue")
+        clickedPattern.push("blue")
+        playSound("blue");
+        checkSequence();
+    })
+
+
+    function checkSequence() {
+        var lol = 0;
+        for (var i = 0; i < clickedPattern.length; i++) {
+            if (colorPattern[i] != clickedPattern[i]) {
+                lol = 0;
+                break;
+            }
+            else if (clickedPattern.length == colorPattern.length) {
+                lol = 1;
+            }
+        }
+        if (lol == 1)
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        else {
+            gameOver();
+            return;
+        }
+
+    }
+
+    function playSound(buttonColor) {
+        if (buttonColor == "green")
+            greensound.play();
+        else if (buttonColor == "red")
+            redsound.play();
+        else if (buttonColor == "blue")
+            bluesound.play();
+        else
+            yellowsound.play();
+    }
+    function showAnimtion(animatedID) {
+        $(animatedID).addClass("pressed");
         setTimeout(() => {
-            nextSequence();
-        }, 1000);
+            $(animatedID).removeClass("pressed")
+        }, 150);
+    }
 
+    checkIfSubmitted();
 }
-
-
+start();
 function gameOver() {
     setTimeout(() => {
         wrongsound.play();
@@ -88,22 +115,3 @@ function gameOver() {
 
 
 }
-
-function playSound(buttonColor) {
-    if (buttonColor == "green")
-        greensound.play();
-    else if (buttonColor == "red")
-        redsound.play();
-    else if (buttonColor == "blue")
-        bluesound.play();
-    else
-        yellowsound.play();
-}
-function showAnimtion(animatedID) {
-    $(animatedID).addClass("pressed");
-    setTimeout(() => {
-        $(animatedID).removeClass("pressed")
-    }, 150);
-}
-
-checkIfSubmitted();
